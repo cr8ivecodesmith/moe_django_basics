c1374870c25aa715a85ffa7590294af5ec7447de
Matt Lebrun <talk@mattlebrun.com>
2016-07-09 00:38:23 +0800

    Created update note functionality and view

--- djnotes/notes/views.py
+++ djnotes/notes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.http import Http404
 
 from .models import Note
 from .forms import NoteForm
@@ -21,6 +22,32 @@ class Index(View):
     def post(self, request, *args, **kwargs):
         form = NoteForm(request.POST)
         context = self.get_context_data()
+        if form.is_valid():
+            form.save()
+        return render(request, self.template_name, context)
+
+
+class Update(View):
+    template_name = 'notes/update.html'
+
+    def get_context_data(self, **kwargs):
+        context = {}
+        try:
+            note_id = kwargs.get('pk', 0)
+            note = Note.objects.get(id=note_id)
+        except Note.DoesNotExist:
+            raise Http404
+        else:
+            context['note'] = note
+            return context
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        form = NoteForm(request.POST, instance=context.get('note'))
         if form.is_valid():
             form.save()
         return render(request, self.template_name, context)
