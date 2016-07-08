f0a795207b3cb5f3cae0c24c3cd94331534c0672
Matt Lebrun <talk@mattlebrun.com>
2016-07-08 22:47:18 +0800

    Added post request handling and saving of new records

--- djnotes/notes/views.py
+++ djnotes/notes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import View
 
 from .models import Note
+from .forms import NoteForm
 
 
 class Index(View):
@@ -17,3 +18,9 @@ class Index(View):
         context = self.get_context_data()
         return render(request, self.template_name, context)
 
+    def post(self, request, *args, **kwargs):
+        form = NoteForm(request.POST)
+        context = self.get_context_data()
+        if form.is_valid():
+            form.save()
+        return render(request, self.template_name, context)
